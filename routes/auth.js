c21b04aa524562d066b371064d10bbe8f7fbfbc5
Ankush Thakur <atpara99@gmail.com>
2023-04-24 14:30:53 +0530

    Code pushed

--- routes/auth.js
+++ routes/auth.js
@@ -1,40 +1,34 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { check, body, validationResult } = require("express-validator");
+const { check, body, validationResult } = require('express-validator');
 
-const { signout, signup, signin, isSignedIn } = require("../controllers/auth");
+const { signout, signup, signin, isSignedIn } = require('../controllers/auth');
 
-// TODO: SIGNUP POST Route
+// TODO: SIGNUP POST Routes
 router.post(
-  "/signup",
+  '/signup',
   [
-    check("name")
-      .isLength({ min: 3 })
-      .withMessage("Name must have 3 Characters"),
-    check("email", "Email is Required").isEmail(),
-    check("password")
-      .isLength({ min: 4 })
-      .withMessage("Password should be atleast of 4 Characters"),
+    check('name').isLength({ min: 3 }).withMessage('Name must have 3 Characters'),
+    check('email', 'Email is Required').isEmail(),
+    check('password').isLength({ min: 4 }).withMessage('Password should be atleast of 4 Characters'),
   ],
   signup
 );
 
 // TODO: SIGNIN POST Route
 router.post(
-  "/signin",
+  '/signin',
   [
-    check("email").isEmail().withMessage("Email is Required"),
-    check("password")
-      .isLength({ min: 1 })
-      .withMessage("Password Field Required"),
+    check('email').isEmail().withMessage('Email is Required'),
+    check('password').isLength({ min: 1 }).withMessage('Password Field Required'),
   ],
   signin
 );
 
 // TODO: SIGNOUT GET ROUTE
-router.get("/signout", signout);
+router.get('/signout', signout);
 
-router.get("/testRoute", isSignedIn, (req, res) => {
+router.get('/testRoute', isSignedIn, (req, res) => {
   res.json(req.auth);
 });
 
